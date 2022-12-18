15d1d6af12e0c143c87e1bf922fac88d3d0008f9
ShitalKarad <shitalkarad24@gmail.com>
2022-12-18 15:44:35 +0530

    Added:uc1 of day_13

---
+++ com/company/PrintArray.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class PrintArray {
+    public static void toPrintArray(Integer[] inputArray){
+        for (int element:inputArray) {
+            System.out.print(element+",");
+        }
+        System.out.println();
+    }
+    public static void toPrintArray(Double[] inputArray){
+        for (double element:inputArray) {
+            System.out.print(element+",");
+        }
+        System.out.println();
+    }
+    public static void toPrintArray(Character[] inputArray){
+        for (char element:inputArray) {
+            System.out.print(element+",");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Integer[] intArray ={1,2,3,4,5,7,8,9};
+        Double[] doubleArray ={1.2,3.2,3.4,4.5,5.2,7.8};
+        Character[] charArray ={'A','C','D','W','Q'};
+        toPrintArray(intArray);
+        toPrintArray(doubleArray);
+        toPrintArray(charArray);
+    }
+}
+
