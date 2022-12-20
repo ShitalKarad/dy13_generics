4d3708c0369a385d305722a4b58ba27b84ee3e2f
ShitalKarad <shitalkarad24@gmail.com>
2022-12-20 12:05:00 +0530

    Added:Day13 uc1 print array of element

--- com/company/PrintArray.java
+++ com/company/PrintArray.java
@@ -13,13 +13,12 @@ public class PrintArray {
         }
         System.out.println();
     }
-    public static void toPrintArray(Character[] inputArray){
-        for (char element:inputArray) {
-            System.out.print(element+",");
+    public static void toPrintArray(Character[] inputArray) {
+        for (char element : inputArray) {
+            System.out.print(element + ",");
         }
         System.out.println();
     }
-
     public static void main(String[] args) {
         Integer[] intArray ={1,2,3,4,5,7,8,9};
         Double[] doubleArray ={1.2,3.2,3.4,4.5,5.2,7.8};
